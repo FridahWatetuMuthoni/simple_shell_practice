e8e10e7515f2f12e73fa258218c3a70446141127
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-05-15 21:41:46 -0700

    zombie state

---
+++ forks_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	pid_t pid;
+
+	printf("Before i fork i was one\n");
+
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Unsuccessfull\n");
+	}
+
+	printf("After fork i am two\n");
+
+	return (0);
+}
